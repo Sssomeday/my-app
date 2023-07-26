00d38aa4e2375c2e24973956966b24d8373450e0
JIYUN PARK <dbswlqkr00@gmail.com>
2023-07-26 17:51:19 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+function App(props){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const currDate = new Date();
+
+
+
+  return( 
+
+
+    <div>
+      <h1>Hello JIYUN!</h1>
+      <h2>The time now is {currDate.toLocaleTimeString()}.</h2>
     </div>
   );
+
 }
 
 export default App;
